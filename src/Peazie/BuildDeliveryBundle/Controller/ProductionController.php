73a929a1abf850e6264827dbaeff85441ecf77d8
Wee Keat Chin <weekeat@visualdensity.com>
2013-10-04 10:03:05 +1000

    Cleanup, fixes and new production standing

--- src/Peazie/BuildDeliveryBundle/Controller/ProductionController.php
+++ src/Peazie/BuildDeliveryBundle/Controller/ProductionController.php
@@ -22,6 +22,15 @@ class ProductionController extends Controller
      */
     public function indexAction(Request $r)
     {
+
+	$data = static::getElbs();
+
+	return array(
+	    'data' => $data['LoadBalancerDescriptions']
+	);
+    }//index
+
+    protected function getElbs() {
         $cache = $this->get('delivery.cache');
 
         if( !$data = $cache->get('elbs-list') ) {
@@ -39,9 +48,33 @@ class ProductionController extends Controller
             $cache->set( 'elbs-list', $data, 30*60 );
         }
 
-        return array( 
-            'data' => $data['LoadBalancerDescriptions']
-        );
-    }//index
+	return $data;
+    }
+
+
+    protected function getElbInstances($config = null) {
+	if( !$config ) {
+	    throw new \Exception("No configuration supplied. Please try again");
+	}
+
+	$cache = $this->get('delivery.cache');
+
+	if( !$data = $cache->get('elbs-list') ) {
+	    $params = $this->container->getParameter('aws');
+
+	    $aws = Aws::factory(array(
+		'key'    => $params['access_key_id'],
+		'secret' => $params['access_key_secret'],
+		'region' => $params['default_region']
+	    ));
+
+	    $elb  = $aws->get('elasticloadbalancing');
+	    $elbs = $elb->describeLoadBalancers();
+	    $data = $elbs->toArray();
+	    $cache->set( 'elbs-list', $data, 30*60 );
+	}
+
+	return $data;
+    }
 
 }
